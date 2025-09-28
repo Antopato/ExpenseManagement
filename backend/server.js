fb3c29af1b355a991cb2249d67b8b858a07fbe9d
Antopato <90763997+Antopato@users.noreply.github.com>
2025-09-28 13:34:32 +0200

    Completed user functionalities

--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,7 @@ app.use(express.json());
 connectDB();
 app.use("/api/auth", authRoutes);
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
